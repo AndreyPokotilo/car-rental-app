74f6a5fb7630f19ee0cc29441656f8cc9a8c3a12
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-09-19 21:51:50 +0200

    added routing

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car-rental-shop">
+        <BrowserRouter basename="/car-rental-app">
           <App />
         </BrowserRouter>
       </PersistGate>
