07f53fff66a3c28de32cb95d05af9fcdac9aedc8
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-09-20 20:34:41 +0200

    added filter style

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,93 @@
-import {  FilterContainer } from "./Filter.styled"
-
+import React, { useState } from 'react';
+import {
+  FilterContainer,
+  Form,
+  BrandWrapper,
+  PriceWrapper,
+  MileageWrapper,
+  MileageInputWrapper,
+  LabelBrand,
+  InputBrand,
+  LabelPrice,
+  InputPrice,
+  LabelMileage,
+  InputMileageFrom,
+  InputMileageTo,
+  Button,
+} from './Filter.styled';
 
 export function Filter() {
-    return (<FilterContainer>Filter</FilterContainer>)
-};
+  const [filterValues, setFilterValues] = useState({
+    field1: '',
+    field2: '',
+    field3: '',
+    field4: '',
+  });
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setFilterValues({
+      ...filterValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // onFilterSubmit(filterValues);
+  };
+
+  return (
+    <FilterContainer>
+      <Form onSubmit={handleSubmit}>
+        <BrandWrapper>
+          <LabelBrand htmlFor="field1">Car brand</LabelBrand>
+          <InputBrand
+            type="text"
+            id="field1"
+            name="field1"
+            placeholder="Enter the text"
+            value={filterValues.field1}
+            onChange={handleInputChange}
+          />
+        </BrandWrapper>
+
+        <PriceWrapper>
+          <LabelPrice htmlFor="field2">Price / 1 hour</LabelPrice>
+          <InputPrice
+            type="text"
+            id="field2"
+            name="field2"
+            placeholder="To $"
+            value={filterValues.field2}
+            onChange={handleInputChange}
+          />
+        </PriceWrapper>
+
+        <MileageWrapper>
+          <LabelMileage htmlFor="field3">Сar mileage / km</LabelMileage>
+          <MileageInputWrapper>
+            <InputMileageFrom
+              type="text"
+              id="field3"
+              name="field3"
+              placeholder="From"
+              value={filterValues.field3}
+              onChange={handleInputChange}
+            />
+            <InputMileageTo
+              type="text"
+              id="field3"
+              name="field4"
+              placeholder="To"
+              value={filterValues.field4}
+              onChange={handleInputChange}
+            />
+          </MileageInputWrapper>
+        </MileageWrapper>
+
+        <Button type="submit">Search</Button>
+      </Form>
+    </FilterContainer>
+  );
+}
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -9,5 +9,138 @@ align-items: center;
 margin-left: auto;
 margin-right: auto;
 margin-bottom: 50px;
-background-color: #3f8eb1;
+/* background-color: #3f8eb1; */
+`;
+
+export const Form = styled.form`
+display: flex;
+`;
+
+export const BrandWrapper = styled.div`
+width: 224px;
+height: 100%;
+margin-right: 18px;
+`;
+
+export const LabelBrand = styled.label`
+color: #8A8A89;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: 18px;
+`;
+
+export const InputBrand = styled.input`
+width: 204px;
+height: 46px;
+color: #121417;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 20px;
+border: none;
+border-radius: 14px;
+background-color: #F7F7FB;
+padding-left: 18px;
+margin-top: 8px;
+`;
+
+export const PriceWrapper = styled.div`
+width: 126px;
+margin-right: 18px;
+`;
+
+export const LabelPrice = styled.label`
+color: #8A8A89;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: 18px;
+`;
+
+export const InputPrice = styled.input`
+width: 106px;
+height: 46px;
+color: #121417;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 20px;
+border: none;
+border-radius: 14px;
+background-color: #F7F7FB;
+padding-left: 18px;
+margin-top: 8px;
+`;
+
+export const MileageWrapper = styled.div`
+
+`;
+
+export const MileageInputWrapper = styled.div`
+display: flex;
+`;
+
+export const LabelMileage = styled.label`
+color: #8A8A89;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: 18px;
+`;
+
+export const InputMileageFrom = styled.input`
+width: 133px;
+height: 46px;
+color: #121417;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 20px;
+border: none;
+border-right: 1px solid #e1e1e4;
+border-radius: 14px;
+border-top-right-radius: 0px;
+border-bottom-right-radius: 0px;
+background-color: #F7F7FB;
+padding-left: 24px;
+margin-top: 8px;
+`;
+export const InputMileageTo = styled.input`
+width: 134px;
+height: 46px;
+color: #121417;
+font-size: 18px;
+font-style: normal;
+font-weight: 500;
+line-height: 20px;
+border: none;
+border-radius: 14px;
+border-top-left-radius: 0px;
+border-bottom-left-radius: 0px;
+background-color: #F7F7FB;
+padding-left: 24px;
+margin-top: 8px;
+margin-right: 18px;
+`;
+
+export const Button = styled.button`
+display: flex;
+width: 136px;
+height: 48px;
+border: none;
+justify-content: center;
+align-items: center;
+border-radius: 12px;
+background: #3470FF;
+color: var(--White, #FFF);
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: 20px;
+margin-top: auto;
+cursor: pointer;
+&:hover {
+    background-color: #0B44CD;
+}
 `;
